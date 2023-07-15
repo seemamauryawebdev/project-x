8b2bfeab71720080f9fd9e0ae8528d7e9bfbbedb
seemamauryawebdev <seemamauryadev@gmail.com>
2023-07-15 21:16:21 +0530

    i added home comp to app

--- flixxit/src/App.js
+++ flixxit/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from  './pages/login/Login';
+import Home from  './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
      <Login  />
+     <Home />
     </div>
   );
 }
